ac0e0baf909947be624782c372dcf6649662ac90
Oksana Nichols <oksanahnichols@yahoo.com>
2019-09-12 02:32:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,11 @@
-def introduction(Mergh)
+<<<<<<< HEAD
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
+=======
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def 
+>>>>>>> 3fe58ded0b2c4128a0595cde329d81c0d4e9d6cd
